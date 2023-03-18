7f53329f486d95c99d4989307a0c3daffbfed710
Emerson <emersonnnb@hotmail.com>
2023-03-17 23:41:27 -0300

    criação do projeto e pastas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { ProdutoComponent } from './features/produto/produto.component';
+import { UserComponent } from './features/user/user.component';
+import { VendaComponent } from './features/venda/venda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "produto", component: ProdutoComponent },
+  { path: "user", component: UserComponent },
+  { path: "venda", component: VendaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './features/produto/produto.component';
+import { UserComponent } from './features/user/user.component';
+import { VendaComponent } from './features/venda/venda.component';
+import { LoginComponent } from './features/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    UserComponent,
+    VendaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
